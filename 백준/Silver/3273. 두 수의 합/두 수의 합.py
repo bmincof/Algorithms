dbade09872fb566c1b0f1805d49a53e88315db42
bmincof <104330984+bmincof@users.noreply.github.com>
2023-08-30 00:09:19 +0900

    [Silver III] Title: 두 수의 합, Time: 164 ms, Memory: 125928 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,17 +1,22 @@
-n = int(input())
-nums = list(map(int, input().split()))
-target = int(input())
+from sys import stdin
+
+# n은 안쓰므로 버림
+stdin.readline()
+nums = list(map(int, stdin.readline().split()))
+target = int(stdin.readline())
 
 # [i]번째 숫자가 하나라도 있는지
-is_exist = [False] * 2_000_000
+upper_bound = max(nums) + 1
+is_exist = [False] * upper_bound
 
 # target을 만들 수 있는 쌍의 개수
 count = 0
 
 for num in nums:
+    check = target - num
     # target - num이 있다면 num과 합쳐서 target을 만들 수 있으므로
     # target - num이 있으면 count에 +1
-    count += 1 if target - num > 0 and is_exist[target - num] else 0
+    count += 1 if 0 < check < upper_bound and is_exist[check] else 0
     # num이 있다고 체크하기
     is_exist[num] = True
 
